36a0eef7fe3929061e757a64524338e08e233656
maz-codes <info@misaghazimi.com>
2025-12-16 10:35:34 +1300

    best results yet, better architecture

---
+++ train_phase3.sh
@@ -0,0 +1,22 @@
+python train.py \
+  --epochs 100 \
+  --batch_size 2 \
+  --grad_accum_steps 8 \
+  --word_dropout 0.3 \
+  --seq_len 512 \
+  --d_model 384 \
+  --nhead 8 \
+  --num_layers 6 \
+  --latent_dim 256 \
+  --num_memory_tokens 4 \
+  --lr 1e-4 \
+  --kl_weight 0.02 \
+  --kl_warmup_epochs 20 \
+  --weight_decay 0.01 \
+  --label_smoothing 0.1 \
+  --lr_warmup_epochs 5 \
+  --free_bits 0.1 \
+  --length_loss_weight 0.1 \
+  --grad_clip 1.0 \
+  --output_dir checkpoints_phase3 \
+  --save_every 5
